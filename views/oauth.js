860a802c58387694a4e39f5d833098dacdf5adf5
thungasr <89589.thungathurti@students.itu.edu>
2016-03-26 10:46:14 -0700

    Initial Development
    Share Authentication Portal development files.

---
+++ views/oauth.js
@@ -0,0 +1,29 @@
+var ids = {
+  facebook: {
+    clientID: '959527280828650',
+    clientSecret: '954e690c922e8eed37984645ebe7aec9',
+    callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+  },
+  twitter: {
+    consumerKey: 'get_your_own',
+    consumerSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:1337/auth/twitter/callback"
+  },
+  github: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:1337/auth/github/callback"
+  },
+  google: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
+  },
+  instagram: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: 'http://127.0.0.1:1337/auth/instagram/callback'
+  }
+};
+
+module.exports = ids;
